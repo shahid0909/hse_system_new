7f8e5d39f424bf9856469d3fd4d62dd10ca3f8e7
shahid <shahidr052@gmail.com>
2022-04-10 09:56:58 +0600

    design

--- resources/views/dashboards/admins/meeting/index.blade.php
+++ resources/views/dashboards/admins/meeting/index.blade.php
@@ -24,77 +24,106 @@
                 <p>{{ $message }}</p>
             </div>
         @endif
-        <h3 class="bg bg-success text-center text-white">Generate Meeting</h3>
-        <form method="post" action="{{ route('meeting.store') }}" enctype="multipart/form-data">
-            @csrf
-            <div class="row g-3 mb-3">
-                <div class="col-sm-6">
-                    <label for="item" class="form-label">Meeting Title</label>
-                    <input type="text" name="title" class="form-control col-md-12" placeholder="Enter  Title">
+        <div class="body d-flex py-3">
+            <div class="container-xxl">
+                <div class="row align-items-center">
+                    <div class="border-0 mb-4">
+                        <div
+                            class="card-header py-3 no-bg bg-transparent d-flex align-items-center px-0 justify-content-between border-bottom flex-wrap"
+                        >
+                            <h3 class="fw-bold py-3 mb-0">Generate Meeting</h3>
+                        </div>
+                    </div>
                 </div>
+                <!-- Row end  -->
+                <div class="row clearfix g-3">
+                    <div class="col-lg-4">
+                        <div class="card">
+                            <div
+                                class="card-header py-3 d-flex justify-content-between bg-transparent border-bottom-0"
+                            >
+                                {{--                                <h6 class="mb-0 fw-bold"></h6>--}}
+                            </div>
 
-                <div class="col-sm-6">
-                    <label for="item" class="form-label">Meeting Date</label>
-                    <input type="Date" name="date" class="form-control col-md-12" placeholder="Enter Meeting Date">
+                            <div class="card-body">
+                                <form method="post" action="{{ route('meeting.store') }}" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row g-3 mb-3">
+                                        <div class="col-sm-12">
+                                            <label for="item" class="form-label">Meeting Title</label>
+                                            <input type="text" name="title" class="form-control "
+                                                   placeholder="Enter  Title">
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <label for="item" class="form-label">Meeting Date</label>
+                                            <input type="Date" name="date" class="form-control"
+                                                   placeholder="Enter Meeting Date">
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <label for="item" class="form-label">Meeting Time</label>
+                                            <input type="time" name="time" class="form-control "
+                                                   placeholder="Enter Meeting Date">
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <label for="item" class="form-label">Meeting Venue</label>
+                                            <input type="text" name="venue" class="form-control "
+                                                   placeholder="Enter Venue Name">
+
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <label class="form-label">Meeting Attendance</label>
+                                            <select name="m_attend_id"
+                                                    class="form-control select">
+                                                <option value="">Select Designation</option>
+                                                @foreach ($values as $value)
+                                                    <option value="{{  $value->id  }}">{{ $value->em_name }}</option>
+                                                @endforeach
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary">
+                                        Submit
+                                    </button>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-8">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <table
+                                    id="myProjectTable"
+                                    class="table table-hover datatable align-middle mb-0"
+                                    style="width: 100%"
+                                >
+                                    <thead>
+                                    <tr>
+                                        <th>Sl.</th>
+                                        <th>Title</th>
+                                        <th>Date</th>
+                                        <th>Time</th>
+                                        <th>Venue</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="col-sm-6">
-                    <label for="item" class="form-label">Meeting Time</label>
-                    <input type="time" name="time" class="form-control col-md-12" placeholder="Enter Meeting Date">
-                </div>
-
-                <div class="col-sm-6">
-                    <label for="item" class="form-label">Meeting Venue</label>
-                    <input type="text" name="venue" class="form-control col-md-12" placeholder="Enter Venue Name">
-                </div>
-
-                <div class="col-sm-6">
-                    <label class="form-label">Meeting Attendance</label>
-                    <select name="m_attend_id"
-                            class="form-control col-md-12">
-                        <option value="">Select Designation</option>
-                        @foreach ($values as $value)
-                            <option value="{{  $value->id  }}">{{ $value->em_name }}</option>
-                        @endforeach
-
-                    </select>
-                </div>
-
-                <div class="col-sm-12">
-                    <button type="submit">Submit</button>
-                </div>
-
-            </div>
-
-        </form>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8">
-                    <table
-                        id="myProjectTable"
-                        class="table table-hover datatable align-middle mb-0"
-                        style="width: 100%"
-                    >
-                        <thead>
-                        <tr>
-                            <th>Sl.</th>
-                            <th>Title</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Venue</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-
-                </div>
+                <!-- Row End -->
             </div>
         </div>
-    </div>
 
-    <!-- Row End -->
+
+        <!-- Row End -->
     </div>
 
 
